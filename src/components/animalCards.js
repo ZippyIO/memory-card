4034415b65402ca19f4fbd3c396c53139b3f1459
Nick <ZippyIO@users.noreply.github.com>
2023-01-14 17:14:12 +1100

    Moved ANIMAL_CARDS into own file

---
+++ src/components/animalCards.js
@@ -0,0 +1,78 @@
+import AlpacaImg from '../assets/animals/alpaca.png';
+import BeaverImg from '../assets/animals/beaver.png';
+import BoarImg from '../assets/animals/boar.png';
+import ChickenImg from '../assets/animals/chicken.png';
+import CowImg from '../assets/animals/cow.png';
+import GiraffeImg from '../assets/animals/giraffe.png';
+import KangarooImg from '../assets/animals/kangaroo.png';
+import KoalaImg from '../assets/animals/koala.png';
+import PandaImg from '../assets/animals/panda.png';
+import ParrotImg from '../assets/animals/parrot.png';
+import PigImg from '../assets/animals/pig.png';
+import RaccoonImg from '../assets/animals/raccoon.png';
+
+import SnakeImg from '../assets/animals/snake.png';
+import TigerImg from '../assets/animals/tiger.png';
+import WolfImg from '../assets/animals/wolf.png';
+export const ANIMAL_CARDS = [
+    {
+        type: 'alpaca',
+        img: AlpacaImg,
+    },
+    {
+        type: 'beaver',
+        img: BeaverImg,
+    },
+    {
+        type: 'boar',
+        img: BoarImg,
+    },
+    {
+        type: 'chicken',
+        img: ChickenImg,
+    },
+    {
+        type: 'cow',
+        img: CowImg,
+    },
+    {
+        type: 'giraffe',
+        img: GiraffeImg,
+    },
+    {
+        type: 'kangaroo',
+        img: KangarooImg,
+    },
+    {
+        type: 'koala',
+        img: KoalaImg,
+    },
+    {
+        type: 'panda',
+        img: PandaImg,
+    },
+    {
+        type: 'parrot',
+        img: ParrotImg,
+    },
+    {
+        type: 'pig',
+        img: PigImg,
+    },
+    {
+        type: 'raccoon',
+        img: RaccoonImg,
+    },
+    {
+        type: 'snake',
+        img: SnakeImg,
+    },
+    {
+        type: 'tiger',
+        img: TigerImg,
+    },
+    {
+        type: 'wolf',
+        img: WolfImg,
+    },
+];

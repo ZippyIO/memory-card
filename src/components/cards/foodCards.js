43bd8a4e27325d82e2dcd33a91ed6fd381d10d3c
Nick <ZippyIO@users.noreply.github.com>
2023-01-15 18:19:37 +1100

    Added Food Cards

---
+++ src/components/cards/foodCards.js
@@ -0,0 +1,80 @@
+import AubergineImg from '../../assets/food/aubergine.png';
+import AvocadoImg from '../../assets/food/avocado.png';
+import BananaImg from '../../assets/food/banana.png';
+import CarrotImg from '../../assets/food/carrot.png';
+import GrapeImg from '../../assets/food/grape.png';
+import LemonImg from '../../assets/food/lemon.png';
+import LettuceImg from '../../assets/food/lettuce.png';
+import OnionImg from '../../assets/food/onion.png';
+import OrangeImg from '../../assets/food/orange.png';
+import PearImg from '../../assets/food/pear.png';
+import PineappleImg from '../../assets/food/pineapple.png';
+import PumpkinImg from '../../assets/food/pumpkin.png';
+import RaspberryImg from '../../assets/food/raspberries.png';
+import StrawberryImg from '../../assets/food/strawberry.png';
+import WatermelonImg from '../../assets/food/watermelon.png';
+
+const FOOD_CARDS = [
+    {
+        type: 'Aubergine',
+        img: AubergineImg,
+    },
+    {
+        type: 'Avocado',
+        img: AvocadoImg,
+    },
+    {
+        type: 'Banana',
+        img: BananaImg,
+    },
+    {
+        type: 'Carrot',
+        img: CarrotImg,
+    },
+    {
+        type: 'Grape',
+        img: GrapeImg,
+    },
+    {
+        type: 'Lemon',
+        img: LemonImg,
+    },
+    {
+        type: 'Lettuce',
+        img: LettuceImg,
+    },
+    {
+        type: 'Onion',
+        img: OnionImg,
+    },
+    {
+        type: 'Orange',
+        img: OrangeImg,
+    },
+    {
+        type: 'Pear',
+        img: PearImg,
+    },
+    {
+        type: 'Pineapple',
+        img: PineappleImg,
+    },
+    {
+        type: 'Pumpkin',
+        img: PumpkinImg,
+    },
+    {
+        type: 'Raspberry',
+        img: RaspberryImg,
+    },
+    {
+        type: 'Strawberry',
+        img: StrawberryImg,
+    },
+    {
+        type: 'Watermelon',
+        img: WatermelonImg,
+    },
+];
+
+export default FOOD_CARDS;

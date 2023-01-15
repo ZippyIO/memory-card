cb971803c66f5538d1452c62cf575104e6c32a8a
Nick <ZippyIO@users.noreply.github.com>
2023-01-15 18:20:13 +1100

    Moved animalCards

--- src/components/cards/animalCards.js
+++ src/components/cards/animalCards.js
@@ -1,20 +1,20 @@
-import AlpacaImg from '../assets/animals/alpaca.png';
-import BeaverImg from '../assets/animals/beaver.png';
-import BoarImg from '../assets/animals/boar.png';
-import ChickenImg from '../assets/animals/chicken.png';
-import CowImg from '../assets/animals/cow.png';
-import GiraffeImg from '../assets/animals/giraffe.png';
-import KangarooImg from '../assets/animals/kangaroo.png';
-import KoalaImg from '../assets/animals/koala.png';
-import PandaImg from '../assets/animals/panda.png';
-import ParrotImg from '../assets/animals/parrot.png';
-import PigImg from '../assets/animals/pig.png';
-import RaccoonImg from '../assets/animals/raccoon.png';
-import SnakeImg from '../assets/animals/snake.png';
-import TigerImg from '../assets/animals/tiger.png';
-import WolfImg from '../assets/animals/wolf.png';
+import AlpacaImg from '../../assets/animals/alpaca.png';
+import BeaverImg from '../../assets/animals/beaver.png';
+import BoarImg from '../../assets/animals/boar.png';
+import ChickenImg from '../../assets/animals/chicken.png';
+import CowImg from '../../assets/animals/cow.png';
+import GiraffeImg from '../../assets/animals/giraffe.png';
+import KangarooImg from '../../assets/animals/kangaroo.png';
+import KoalaImg from '../../assets/animals/koala.png';
+import PandaImg from '../../assets/animals/panda.png';
+import ParrotImg from '../../assets/animals/parrot.png';
+import PigImg from '../../assets/animals/pig.png';
+import RaccoonImg from '../../assets/animals/raccoon.png';
+import SnakeImg from '../../assets/animals/snake.png';
+import TigerImg from '../../assets/animals/tiger.png';
+import WolfImg from '../../assets/animals/wolf.png';
 
-export const ANIMAL_CARDS = [
+const ANIMAL_CARDS = [
     {
         type: 'alpaca',
         img: AlpacaImg,
@@ -76,3 +76,5 @@ export const ANIMAL_CARDS = [
         img: WolfImg,
     },
 ];
+
+export default ANIMAL_CARDS;

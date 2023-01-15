96da557999229ae2d383df88ed8129e4ea91a120
Nick <ZippyIO@users.noreply.github.com>
2023-01-15 18:30:35 +1100

    Moved Card functions from App to Cards
    Refactored and added functions to App and Cards components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Cards from './components/Cards';
+import Menu from './components/Menu';
+import RoundAnnouncement from './components/RoundAnnouncement';
 import Scoreboard from './components/Scoreboard';
 
 const App = () => {
+    const [cardSet, setCardSet] = useState('animals');
     const [score, setScore] = useState(0);
     const [bestScore, setBestScore] = useState(0);
-    const [clickedCards, setClickedCards] = useState([]);
+    const [roundStatus, setRoundStatus] = useState(null);
 
-    const clickCard = (e) => {
-        const targetCard = e.currentTarget.id;
-        if (clickedCards.find((element) => element === targetCard)) {
-            setBestScore(bestScore < score ? score : bestScore);
-            setScore(0);
-            setClickedCards([]);
-        } else {
-            setScore(score + 1);
-            setClickedCards((prevArray) => [...prevArray, targetCard]);
+    useEffect(() => {
+        if (score >= 15) {
+            resetGame();
+            setRoundStatus(true);
         }
+    }, [score]);
+
+    const updateScore = () => {
+        setScore(score + 1);
+        setRoundStatus(null);
+    };
+
+    const updateBestScore = () => {
+        setBestScore(bestScore < score ? score : bestScore);
+        setScore(0);
+        setRoundStatus(false);
+    };
+
+    const resetGame = () => {
+        setScore(0);
+        setBestScore(0);
+    };
+
+    const clickCardSet = (e) => {
+        const set = e.currentTarget.dataset.cardset;
+        resetGame();
+        setRoundStatus(null);
+        setCardSet(set);
     };
 
     return (
         <div id="app">
-            <div id="main">
-                <h1>Memory Game</h1>
-                <Scoreboard currentScore={score} bestScore={bestScore} />
-                <Cards click={clickCard} />
+            <div>
+                <Menu cardset={clickCardSet} />
+                <RoundAnnouncement status={roundStatus} />
+
+                <div id="main">
+                    <h1>Memory Game</h1>
+                    <Scoreboard currentScore={score} bestScore={bestScore} />
+                    <Cards
+                        updateScore={updateScore}
+                        updateBestScore={updateBestScore}
+                        cardset={cardSet}
+                    />
+                </div>
             </div>
             <div id="footer">
-                <a href="https://www.flaticon.com/packs/animals-148">
-                    Animal icons created by Freepik - Flaticon
+                <a href="https://www.flaticon.com/authors/freepik">
+                    Icons created by Freepik - Flaticon
                 </a>
             </div>
         </div>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,9 @@
-import React, { useState } from 'react';
-import { ANIMAL_CARDS } from './animalCards';
+import React, { useEffect, useState } from 'react';
+import '../styles/Cards.css';
+import ANIMAL_CARDS from './cards/animalCards';
+import CHARACTER_CARDS from './cards/characterCards';
+import DOG_CARDS from './cards/dogCards';
+import FOOD_CARDS from './cards/foodCards';
 
 const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -13,6 +17,39 @@ const shuffleArray = (array) => {
 
 const Cards = (props) => {
     const [cards, setCards] = useState(shuffleArray([...ANIMAL_CARDS]));
+    const [clickedCards, setClickedCards] = useState([]);
+
+    useEffect(() => {
+        setClickedCards([]);
+        if (props.cardset === 'animals') {
+            setCards(() => shuffleArray([...ANIMAL_CARDS]));
+        } else if (props.cardset === 'dogs') {
+            setCards(() => shuffleArray([...DOG_CARDS]));
+        } else if (props.cardset === 'food') {
+            setCards(() => shuffleArray([...FOOD_CARDS]));
+        } else if (props.cardset === 'characters') {
+            setCards(() => shuffleArray([...CHARACTER_CARDS]));
+        }
+    }, [props.cardset]);
+
+    const clickCard = (e) => {
+        const targetCard = e.currentTarget.id;
+        if (clickedCards.find((element) => element === targetCard)) {
+            incorrectCardClick();
+        } else {
+            correctCardClick(targetCard);
+        }
+    };
+
+    const correctCardClick = (card) => {
+        props.updateScore();
+        setClickedCards((prevArray) => [...prevArray, card]);
+    };
+
+    const incorrectCardClick = () => {
+        props.updateBestScore();
+        setClickedCards([]);
+    };
 
     const shuffle = () => {
         setCards(() => shuffleArray([...cards]));
@@ -23,11 +60,11 @@ const Cards = (props) => {
             {cards.map((card) => {
                 return (
                     <div
-                        id={`animal-card-${card.type}`}
-                        className="animal-card"
-                        key={`animal-card-${card.type}`}
+                        id={`card-${card.type}`}
+                        className="card"
+                        key={`card-${card.type}`}
                         onClick={(e) => {
-                            props.click(e);
+                            clickCard(e);
                             shuffle();
                         }}
                     >

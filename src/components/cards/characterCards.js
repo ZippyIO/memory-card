261ca6f22d34f01bf11617087adcba8457d1e65f
Nick <ZippyIO@users.noreply.github.com>
2023-01-15 18:20:00 +1100

    Added Character Cards

---
+++ src/components/cards/characterCards.js
@@ -0,0 +1,80 @@
+import AlchemistImg from '../../assets/characters/alchemist.png';
+import CosmonautImg from '../../assets/characters/cosmonaut.png';
+import CyborgImg from '../../assets/characters/cyborg.png';
+import KingImg from '../../assets/characters/king.png';
+import KnightImg from '../../assets/characters/knight.png';
+import KnightTwoImg from '../../assets/characters/knightTwo.png';
+import MagicianImg from '../../assets/characters/magician.png';
+import MagicianTwoImg from '../../assets/characters/magicianTwo.png';
+import MilitaryImg from '../../assets/characters/military.png';
+import MilitaryTwoImg from '../../assets/characters/militaryTwo.png';
+import QueenImg from '../../assets/characters/queen.png';
+import RobotImg from '../../assets/characters/robot.png';
+import SamuraiImg from '../../assets/characters/samurai.png';
+import VikingImg from '../../assets/characters/viking.png';
+import WarriorImg from '../../assets/characters/warrior.png';
+
+const CHARACTER_CARDS = [
+    {
+        type: 'Alchemist',
+        img: AlchemistImg,
+    },
+    {
+        type: 'Cosmonaut',
+        img: CosmonautImg,
+    },
+    {
+        type: 'Cyborg',
+        img: CyborgImg,
+    },
+    {
+        type: 'King',
+        img: KingImg,
+    },
+    {
+        type: 'Knight',
+        img: KnightImg,
+    },
+    {
+        type: 'KnightTwo',
+        img: KnightTwoImg,
+    },
+    {
+        type: 'Magician',
+        img: MagicianImg,
+    },
+    {
+        type: 'MagicianTwo',
+        img: MagicianTwoImg,
+    },
+    {
+        type: 'Military',
+        img: MilitaryImg,
+    },
+    {
+        type: 'MilitaryTwo',
+        img: MilitaryTwoImg,
+    },
+    {
+        type: 'Queen',
+        img: QueenImg,
+    },
+    {
+        type: 'Robot',
+        img: RobotImg,
+    },
+    {
+        type: 'Samurai',
+        img: SamuraiImg,
+    },
+    {
+        type: 'Viking',
+        img: VikingImg,
+    },
+    {
+        type: 'Warrior',
+        img: WarriorImg,
+    },
+];
+
+export default CHARACTER_CARDS;

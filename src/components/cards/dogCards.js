e2904bb1507226ee27c4b798a51ae5d58f4c3ab7
Nick <ZippyIO@users.noreply.github.com>
2023-01-15 18:18:48 +1100

    Added Dog Cards

---
+++ src/components/cards/dogCards.js
@@ -0,0 +1,80 @@
+import AkitaImg from '../../assets/dogs/akita.png';
+import BorderCollieImg from '../../assets/dogs/border-collie.png';
+import BoxerImg from '../../assets/dogs/boxer.png';
+import BullTerrierImg from '../../assets/dogs/bull-terrier.png';
+import ChowChowImg from '../../assets/dogs/chow-chow.png';
+import DobermanImg from '../../assets/dogs/doberman.png';
+import EnglishMastiffImg from '../../assets/dogs/english-mastiff.png';
+import FrenchBulldogImg from '../../assets/dogs/french-bulldog.png';
+import JackRussellTerrierImg from '../../assets/dogs/jack-russell-terrier.png';
+import PugImg from '../../assets/dogs//pug.png';
+import RottweilerImg from '../../assets/dogs/rottweiler.png';
+import SchnauzerImg from '../../assets/dogs/schnauzer.png';
+import SharPeiImg from '../../assets/dogs/shar-pei.png';
+import ShihTzuImg from '../../assets/dogs/shih-tzu.png';
+import YorkshireTerrierImg from '../../assets/dogs/yorkshire-terrier.png';
+
+const DOG_CARDS = [
+    {
+        type: 'akita',
+        img: AkitaImg,
+    },
+    {
+        type: 'border-collie',
+        img: BorderCollieImg,
+    },
+    {
+        type: 'boxer',
+        img: BoxerImg,
+    },
+    {
+        type: 'bull-terrier',
+        img: BullTerrierImg,
+    },
+    {
+        type: 'chow-chow',
+        img: ChowChowImg,
+    },
+    {
+        type: 'doberman',
+        img: DobermanImg,
+    },
+    {
+        type: 'english-mastiff',
+        img: EnglishMastiffImg,
+    },
+    {
+        type: 'french-bulldog',
+        img: FrenchBulldogImg,
+    },
+    {
+        type: 'jack-russell',
+        img: JackRussellTerrierImg,
+    },
+    {
+        type: 'pug',
+        img: PugImg,
+    },
+    {
+        type: 'rottweiler',
+        img: RottweilerImg,
+    },
+    {
+        type: 'schnauzer',
+        img: SchnauzerImg,
+    },
+    {
+        type: 'shar-pei',
+        img: SharPeiImg,
+    },
+    {
+        type: 'shih-tzu',
+        img: ShihTzuImg,
+    },
+    {
+        type: 'yorkshire-terrier',
+        img: YorkshireTerrierImg,
+    },
+];
+
+export default DOG_CARDS;
